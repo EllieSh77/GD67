5cf5ff44371d2e3046ab18dc8f14075770a489d9
Ellie Shariari Ahmadi <shahriari_Ahmadi_Elh@student.smc.edu>
2019-10-08 13:48:05 -0700

    Check out Form
    The billing address section form checkout  and shipping

---
+++ week-6 in class/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      //$("#mainTitle").mainclass("pink");
+
+      $("colorChange").click(changeColor);
+
+      function changeColor()
+        $("#mainTitle").toggleclass
+         if( $("#mainTitle").hasclass("pink")
+          $("#mainTitle").removeclass("pink")
+          $("#mainTitle").addclass("blue")
+})
+         else if( $"")
