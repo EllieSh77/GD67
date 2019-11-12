9e9b2c6133ba0becd3313ee8d592a24d5a59d0a0
Ellie Shariari Ahmadi <shahriari_Ahmadi_Elh@student.smc.edu>
2019-11-12 13:53:26 -0800

    week-11
    Canvas

---
+++ week 11- canves /canvas.js
@@ -0,0 +1,55 @@
+var Airplain = new Image();
+function init() {
+  Airplain.src = 'https://www.searchpng.com/wp-content/uploads/2019/01/Planes-PNG-image-1-715x241.png';
+   window.requestAnimationFrame(draw);
+}
+
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0, 0,400, 600); // clear canvas
+
+  ctx.fillStyle = "#b40ef7";
+  ctx.strokeStyle = "#b20ef7";
+  ctx.save();
+  ctx.translate(150, 150);
+
+  
+
+  // Canvas
+  ctx.save();
+  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
+  ctx.translate(0, 28.5);
+  ctx.drawImage(Airplain, 3.5, 3.5);
+  ctx.restore();
+
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); 
+  ctx.stroke();
+
+  ctx.drawImage(Airplain, 0, 0, 300, 300);
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
+
+        const canvas = document.getElementById("myCanvas");
+        const ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = "#bca079";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.shadowBlur = 10;
+        ctx.shadowOffsetX = 6;
+        ctx.shadowColor = "#f6d2bf";
+        ctx.fillStyle = "#f0e9c0";
+        ctx.fillRect(40, 40, 300, 250);
+
+        ctx.font = "34px Arial";
+ctx.strokeText("Cabo San Lucas", 65, 160);
+
+
