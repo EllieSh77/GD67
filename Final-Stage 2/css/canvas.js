38186c7981a9a1e7a7d54c9c596f11e5108616ca
Ellie Shariari Ahmadi <shahriari_Ahmadi_Elh@student.smc.edu>
2019-12-10 15:37:06 -0800

    Final-2
    fixed

--- Final-Stage 2/css/canvas.js
+++ Final-Stage 2/css/canvas.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 var ballX = canvas.width/2;
 var ballY = canvas.height/2;
-var ballColor = 'rgb(0, 155, 155)';
+var ballColor = "#292621";
 var ballRadius = 50;
 
 var speedX = 5;
@@ -13,7 +13,7 @@ var directionDown = true;
 var directionRight = true;
 
 function animate(){
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+    ctx.fillStyle = "#786f5e";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
